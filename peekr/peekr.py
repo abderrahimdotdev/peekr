084ce21d5be0ee76f458cd877bf1fdc4fd2e38fd
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-30 17:40:42 +0100

    Add print_help() method

--- peekr/peekr.py
+++ peekr/peekr.py
@@ -1,5 +1,27 @@
+#!/usr/bin/env python
+
+
+def print_help():
+    print("""
+    Search text in multiple images using tesseract (an OCR — Optical Character Recognition — tool powered by Google.
+
+    Usage:
+        peekr.py [options]
+
+    Options:
+        -h, --help  display this help
+        -I, --interactive   Run in interactive mode
+        -d, --directory Specify the target directory to be searched
+        -c, --case-sensitive  Perform a case-sensitive search
+        -L, --lang  Specify the language to be used in search (Default is English)
+        -l, --ui-lang Specify the language to use when displaying messages
+        -r, --recursive Recursively search subdirectories
+        -o, --output    Create a folder in the specified location and copy the pictures found into it  
+        -k, --keyword A keyword to search for 
+    """)
+            
 def main():
-    print("Hello from peekr!")
+    print_help()
 
 
 if __name__ == "__main__":
